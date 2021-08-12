1f86c23320ee4950747a8088fb4482d4704e3fd7
JUNE <63999666+DevelopJun@users.noreply.github.com>
2021-08-12 21:35:44 +0900

    Add files via upload

--- zoom.js/src/server.js
+++ zoom.js/src/server.js
@@ -1,3 +1,6 @@
+/*
+** 서버 실행하는 방법은 npm run dev 
+*/
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
@@ -20,15 +23,19 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app); // http 서버이고, 
 const wws = new WebSocket.Server({ server }) // webSocket 서버인거지,  이제 그럼 저 위에 http 서버가 시작되면, websocket 서버도 실행된다는 의미로 { server }을 넣게 된거임.
 
+
+const sockets = [];
+
 wws.on("connection", (socket)=>{
+    sockets.push(socket);
     console.log("Connected to Browser");
     socket.on("close", ()=>{
         console.log("Disconnected from Browser ❌ ");
     });
     socket.on("message", (message) =>{
-        console.log(message.toString('utf8'));
+        // console.log(message.toString('utf8')); // tostring 사용 안하면 에러 발생 제대로 data 가지 않는다.
+        sockets.forEach((aSocket) => aSocket.send(message.toString('utf8')));
     });
-    socket.send("pogba!!");
 }); // 이거 js 이벤트 호출이랑 유사하고, 저 connection 왔을때 func 주는거고, 몇가지 없으니까 쉬운 개념 
 
 
